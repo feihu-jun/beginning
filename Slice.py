9ffb5c9b5cff2ac97eb04c64d6e2dd59decc3635
wen <410401925@qq.com>
2017-02-27 22:39:26 +0800

    diary

---
+++ Slice.py
@@ -0,0 +1,7 @@
+#用切片
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+r = []
+n = 3
+for i in range(n):#循环n次
+    r.append(L[i])#每次在后加l中元素
+print(r)
---
+++ list13579.py
@@ -0,0 +1,7 @@
+#构造一个列表
+L = []
+n = 1
+while n <= 99:
+    L.append(n)#在列表尾部加
+    n = n + 2
+print(L)
